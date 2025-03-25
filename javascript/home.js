925b23f2718fa2f2d9e3a1cd427db846ca6381f7
Thomas <thomasam04@outlook.com>
2025-03-25 20:11:23 +0000

    fixed errors made while tidying

---
+++ javascript/home.js
@@ -0,0 +1,7 @@
+    //alert that appears on opening page
+    alert("Hello! make sure to record your current mood with the mood tracker :)");
+    
+    //function to provide feedback on button click 
+    function feedback() {
+        alert("Response recorded!");
+    }
